607e2676729c8a87af68f2d0c105b852b53e9e92
stirwin work local <stirwincastro@gmail.com>
2025-11-18 18:57:42 -0500

    final v2

--- components/CompanyInfo.tsx
+++ components/CompanyInfo.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { motion } from "framer-motion"
-import { MessageCircle, Phone } from "lucide-react"
+import { MessageCircle } from "lucide-react"
 import Image from "next/image"
 
 interface CompanyInfoProps {
